c72368bf70d6ba5887ce6ebf421cd8f0285a910b
Sauravkumar8684 <sauravks1234@gmail.com>
2026-03-26 19:50:59 +0530

    update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://shopease-44cb.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 //  Request Interceptor (token attach)
